1e16be5909e075c1b61b3efcb3e0ec8700cedc61
Matt Myers <matthew_myers@bose.com>
2020-08-25 21:40:22 -0600

    Add reuse port/address for Windows and Linux
    Add OS-specific port reuse handling for Windows sockets. Also add port reuse for Linux, which supports different options depending on the kernel version.

--- lib/mdns_lite/responder.ex
+++ lib/mdns_lite/responder.ex
@@ -20,6 +20,7 @@ defmodule MdnsLite.Responder do
   @mdns_port 5353
   @sol_socket 0xFFFF
   @so_reuseport 0x0200
+  @so_reuseaddr 0x0004
 
   defmodule State do
     @moduledoc false
@@ -225,13 +226,33 @@ defmodule MdnsLite.Responder do
     ] ++ reuse_port()
   end
 
-  defp reuse_port() do
+  def reuse_port() do
     case :os.type() do
+      {:unix, :linux} ->
+        reuse_port_linux()
+
       {:unix, os_name} when os_name in [:darwin, :freebsd, :openbsd, :netbsd] ->
-        [{:raw, @sol_socket, @so_reuseport, <<1::native-32>>}]
+        get_reuse_port()
+
+      {:win32, _unused} ->
+        get_reuse_address()
 
       _ ->
         []
     end
   end
+
+  defp reuse_port_linux() do
+    case :os.version() do
+      {major, minor, _} when major > 3 or (major == 3 and minor >= 9) ->
+        get_reuse_port()
+
+      _before_3_9 ->
+        get_reuse_address()
+    end
+  end
+
+  defp get_reuse_port(), do: [{:raw, @sol_socket, @so_reuseport, <<1::native-32>>}]
+
+  defp get_reuse_address(), do: [{:raw, @sol_socket, @so_reuseaddr, <<1::native-32>>}]
 end
