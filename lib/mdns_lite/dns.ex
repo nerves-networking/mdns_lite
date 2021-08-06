a313974b4c7245c986c6ac685468aa757cb5e294
Frank Hunleth <fhunleth@troodon-software.com>
2021-08-05 22:45:26 -0400

    Cleanup DNS record definitions

--- lib/mdns_lite/dns.ex
+++ lib/mdns_lite/dns.ex
@@ -1,10 +1,15 @@
 defmodule MdnsLite.DNS do
+  @moduledoc """
+  Bring Erlang's DNS record definitions into Elixir
+  """
   import Record, only: [defrecord: 2]
 
-  defrecord :dns_rec, Record.extract(:dns_rec, from_lib: "kernel/src/inet_dns.hrl")
-  defrecord :dns_header, Record.extract(:dns_header, from_lib: "kernel/src/inet_dns.hrl")
-  defrecord :dns_query, Record.extract(:dns_query, from_lib: "kernel/src/inet_dns.hrl")
-  defrecord :dns_rr, Record.extract(:dns_rr, from_lib: "kernel/src/inet_dns.hrl")
+  @inet_dns "kernel/src/inet_dns.hrl"
+
+  defrecord :dns_rec, Record.extract(:dns_rec, from_lib: @inet_dns)
+  defrecord :dns_header, Record.extract(:dns_header, from_lib: @inet_dns)
+  defrecord :dns_query, Record.extract(:dns_query, from_lib: @inet_dns)
+  defrecord :dns_rr, Record.extract(:dns_rr, from_lib: @inet_dns)
 
   @type dns_query :: record(:dns_query, [])
   @type dns_rr :: record(:dns_rr, [])
