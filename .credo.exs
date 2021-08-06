2a701807d3fe3be73b6383aa4772655f0b6ee106
Frank Hunleth <fhunleth@troodon-software.com>
2021-08-05 22:45:05 -0400

    Refresh credo config/format

--- .credo.exs
+++ .credo.exs
@@ -1,11 +1,9 @@
-# config/.credo.exs
+# .credo.exs
 %{
   configs: [
     %{
       name: "default",
       checks: [
-        {Credo.Check.Refactor.MapInto, false},
-        {Credo.Check.Warning.LazyLogging, false},
         {Credo.Check.Readability.LargeNumbers, only_greater_than: 86400},
         {Credo.Check.Readability.ParenthesesOnZeroArityDefs, parens: true}
       ]
