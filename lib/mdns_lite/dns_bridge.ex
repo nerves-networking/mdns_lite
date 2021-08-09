75640128a84244bd74c15810a1e397133b8fcf74
Frank Hunleth <fhunleth@troodon-software.com>
2021-08-08 21:46:38 -0400

    Support queries via bridge

--- lib/mdns_lite/dns_bridge.ex
+++ lib/mdns_lite/dns_bridge.ex
@@ -22,7 +22,7 @@ defmodule MdnsLite.DNSBridge do
   use GenServer
   require Logger
 
-  alias MdnsLite.{IfInfo, Options, TableServer}
+  alias MdnsLite.Options
   import MdnsLite.DNS
 
   ##############################################################################
@@ -60,7 +60,8 @@ defmodule MdnsLite.DNSBridge do
          # qr is the query/response flag; false (0) = query, true (1) = response
          dns_header(qr: false) <- header do
       # only respond to the first query
-      result = TableServer.query(hd(qdlist), %IfInfo{ipv4_address: {127, 0, 0, 1}})
+
+      result = MdnsLite.query(hd(qdlist))
 
       send_response(qdlist, result, dns_record, {src_ip, src_port}, state)
     end
